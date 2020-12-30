6a4a363af1f540ecbb8f0fb4c51990cc7f25fa36
csandeepreddy <sandeepreddy.c6543@gmail.com>
2020-12-30 17:17:17 +0530

    add

---
+++ sample.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int
+	var j int
+	i=4
+	j=6
+	fmt.Println(i * j)
+
+// without var and declaring variables
+
+/*name :="sandeep"
+fmt.Println("my name is " + name)
+
+//without var and int
+
+name="suresh"
+k :=24
+fmt.Printf("%v, %T", k,name)
+
+//percentage of printf value = %t and %var name type
+
+var (
+name string="sandeep"
+c int=999
+)
+
+fmt.Println(name , c)
+
+var m int=65
+var sname string="sudheer"
+sname=string(m)
+fmt.Println("my value is ",m,"and my name is "+sname)
+
+
+//
+
+var m int=67
+var sname string
+ sname=strconv.Itoa(m)
+ fmt.Println(sname)
+
+//captial i=integer and a standsfor= ascii */
+
+var z bool=true
+
+fmt.Printf("%T , %v\n" ,z , z )
+
+
+
+
+}
