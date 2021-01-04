4b95919f38863333e8c44f3c1215b06964d15410
csandeepreddy <sandeepreddy.c6543@gmail.com>
2021-01-04 16:55:13 +0530

    looping

---
+++ looping.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main()  {
+
+/* for i :=0 ; i < 5 ; i++ {
+   fmt.Println(i)
+} */
+
+/* for  i , j:=0,0;  i<5;  i, j = i+1 ,j+3 {
+  fmt.Println(i,j)
+} */
+
+
+/* i :=0
+for  ; i < 5 ; i++ {
+   fmt.Println(i)
+}
+
+fmt.Println(i) */
+
+
+/* i :=0
+for  ; i < 5 ; i++ {
+   fmt.Println(i)
+   i++
+} */
+
+
+ /* for i:=0 ; i < 10 ; i++ {
+   if i&2 ==0 {
+     continue
+   }
+
+fmt.Println(i)
+} */
+
+ /* s:= "sandeep"
+for k , v := range s {
+  fmt.Println(k,string(v))
+} */
+
+s:= []int{1,2,3,4,5,6}
+for k , v := range s {
+  fmt.Println(k,v)              // key values
+}
+
+
+
+
+
+
+
+
+
+
+}
