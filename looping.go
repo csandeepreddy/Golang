9761a005864c9905c97d2dd6e9f016852418e3e8
csandeepreddy <sandeepreddy.c6543@gmail.com>
2021-01-05 21:53:55 +0530

    interface & looping

--- looping.go
+++ looping.go
@@ -6,9 +6,9 @@ import (
 
 func main()  {
 
-/* for i :=0 ; i < 5 ; i++ {
+ for i :=0 ; i < 5 ; i++ {
    fmt.Println(i)
-} */
+} 
 
 /* for  i , j:=0,0;  i<5;  i, j = i+1 ,j+3 {
   fmt.Println(i,j)
@@ -43,10 +43,10 @@ for k , v := range s {
   fmt.Println(k,string(v))
 } */
 
-s:= []int{1,2,3,4,5,6}
+/*  s:= []int{1,2,3,4,5,6}
 for k , v := range s {
   fmt.Println(k,v)              // key values
-}
+}                     */
 
 
 
